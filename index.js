0e0d6f77053f6c4c6ce95a7e0b4bb425f8e75047
roy bae <roybae@roys-Laptop.home>
2022-11-28 15:45:28 -0800

    Feature - Backend Complete

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const { ApolloServer } = require('apollo-server');
-const mongoose = require('mongoose')
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 
-
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
-const { MONGODB } = require('./config.js')
-
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+  context: ({ req }) => ({ req }),
+});
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to MongoDB')
-    return server.listen({port : 5050});
+    console.log("Connected to MongoDB");
+    return server.listen({ port: 5050 });
   })
   .then((res) => {
     console.log(`server is running on ${res.url}`);
-  })
+  });
